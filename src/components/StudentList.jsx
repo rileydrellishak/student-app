1e21b3743850695484e0c6da6aa0f9d84319aeb0
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-04 14:15:46 -0500

    styling components

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,5 +1,19 @@
+import './StudentList.css';
+
 const StudentList = () => {
-  return <h2>Student List</h2>;
+  const headingClass = 'student-list__heading';
+  const listClass = 'student-list';
+  
+  return (
+    <section>
+      <h2 className={headingClass}>Student List</h2>
+      <ul className={listClass}>
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
 };
 
 export default StudentList;
