5de3f5e2df9058f6e65b04be720281ed699dd834
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-08 10:14:49 -0500

    array of Student components

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -5,13 +5,17 @@ const StudentList = () => {
   const headingClass = 'student-list__heading';
   const listClass = 'student-list';
   
+  const studentComponents = [
+    <li><Student name='Riley' email='riley@email.com'></Student></li>,
+    <li><Student name='Maille' email='maille@email.com'></Student></li>,
+    <li><Student name='Bixby' email='bixby@dog.com'></Student></li>
+  ]
+
   return (
     <section>
       <h2 className={headingClass}>Student List</h2>
       <ul className={listClass}>
-        <li><Student name='Riley' email='riley@email.com'></Student></li>
-        <li><Student name='Maille' email='maille@email.com'></Student></li>
-        <li><Student name='Bixby' email='bixby@dog.com'></Student></li>
+        {studentComponents}
       </ul>
     </section>
   );
